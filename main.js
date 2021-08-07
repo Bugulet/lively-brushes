e3158cd61b7b07c36113bb75c2c59c3cd5218135
Eusebiu <eusebiu@totalreality.nl>
2021-08-07 03:16:57 +0300

    added the undo, still a bit glitchy, sometimes undoes 2 steps

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let x = [], y = [], px = [], py = [], lineColor = [];
+let layers = Array(10);
+let currentLayer = 0;
+
+let x = [], y = [], px = [], py = [];
 let vx = [], vy = [];
 
 let particleCount = 400
@@ -9,10 +12,16 @@ let currentBrush = "Default";
 
 let colorpicker;
 
+let strokeColor;
+
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
 
-  canvas.mouseClicked(refreshColors);
+  for (let i = 0; i < layers.length; i++) {
+    layers[i] = createGraphics(windowWidth, windowHeight);
+    layers[i].clear();
+  }
+
 
   colorPicker = createColorPicker('#ff00d4');
   colorPicker.position(0, 0);
@@ -24,41 +33,45 @@ function setup() {
     vy.push(0);
     px.push(0);
     py.push(0);
-    lineColor.push(random(270, 360));
   }
   background(0);
   //colorMode(HSB);
-  blendMode(ADD);
-  
+
 }
-let lineAmount=0;
 function draw() {
+  background(0);
+
 
   fill(255, 255, 255, 255);
-  strokeWeight(0);
   textSize(15);
   text("press space to clear", 0, 40);
   text("press Q to switch brush", 0, 60);
-  // textSize(20)
-  // text(`w:${width} h:${height}`, 0, 90);
-   
-  // stroke(lineColor[0], 70, 5)
-
-  //beginShape(LINES);
 
 
-  strokeWeight(1);
+  layers[currentLayer].blendMode(ADD);
 
-  let strokeColor=colorPicker.color();
-    strokeColor.setAlpha(15);
-    stroke(strokeColor);
+  strokeColor = colorPicker.color();
+  strokeColor.levels[0]/=10;
+  strokeColor.levels[1]/=10;
+  strokeColor.levels[2]/=10;
+  if(strokeColor.levels[0]<5){
+    strokeColor.levels[0]=5;
+  }
+  if(strokeColor.levels[1]<5){
+    strokeColor.levels[1]=5;
+  }
+  if(strokeColor.levels[2]<5){
+    strokeColor.levels[2]=5;
+  }
+  //strokeColor.setAlpha(15);
+  layers[currentLayer].stroke(strokeColor);
 
   for (let i = 0; i < particleCount; i++) {
 
-    
+
     //randomed brush
     //if(Math.abs(mouseX - x[i])>50 && Math.abs(mouseY - y[i])>50) <- cool star stuff
-    if (currentBrush == "Default" ) {
+    if (currentBrush == "Default") {
       vx[i] += (mouseX - x[i]) / (10 + i / 15) + random(-3, 3);
       vy[i] += (mouseY - y[i]) / (10 + i / 15) + random(-3, 3);
     }
@@ -72,39 +85,41 @@ function draw() {
     py[i] = y[i];
     x[i] += vx[i];
     y[i] += vy[i];
-    
+
     if (mouseIsPressed) {
-      //vertex(px[i], py[i]);
-      //vertex(x[i], y[i]);
-       line(px[i], py[i], x[i], y[i]);
-       lineAmount++;
-       line(width - px[i], py[i], width - x[i], y[i]);
+      layers[currentLayer].line(px[i], py[i], x[i], y[i]);
+      layers[currentLayer].line(width - px[i], py[i], width - x[i], y[i]);
     }
     vx[i] *= 0.78;
     vy[i] *= 0.78;
 
   }
-  //endShape();
-  print(lineAmount+"lines");
-}
 
-
-function refreshColors() {
-  let baseNum = random(0, 300);
-  for (let i = 0; i < particleCount; i++) {
-    lineColor[i] = random(baseNum, baseNum + 60);
+  for (let i = 0; i < currentLayer + 1; i++) {
+    image(layers[i], 0, 0);
   }
 }
+
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function keyPressed() {
+  if (key=='z' && currentLayer>1) {
+    layers[currentLayer].clear();
+    currentLayer--;
+    
+    print("juj"+currentLayer);
+  }
   if (keyCode === 32) {
-    blendMode(BLEND);
+    currentLayer=0;
+    print("space pressed");
     background(0);
-    blendMode(ADD);
+    layers.forEach((layer)=>layer.clear());
+    
   }
   if (keyCode === 81) {
     console.warn(currentBrush);
@@ -112,8 +127,22 @@ function keyPressed() {
   }
 }
 
-// function mouseReleased() {
-//   ellipse(mouseX, mouseY, 5, 5);
-//   // prevent default
-//   return false;
-// }
+function mousePressed(){
+  print(currentLayer);
+  if(currentLayer>=layers.length-1){
+    
+    //copy the first layer onto the second, and delete the first one
+    layers[1].copy(layers[0],0,0,windowWidth,windowHeight,0,0,windowWidth,windowHeight); 
+    layers.shift();
+    
+    //since we shifted it, i'll just push a new element like this
+    layers.push(createGraphics(windowWidth,windowHeight));
+    layers[layers.length-1].clear();
+    currentLayer--;
+  }
+  else{
+    layers[currentLayer+1].copy(layers[currentLayer],0,0,windowWidth,windowHeight,0,0,windowWidth,windowHeight); 
+    currentLayer++;
+  }
+}
+
